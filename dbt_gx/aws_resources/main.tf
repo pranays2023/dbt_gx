6f4133dd21b2125e3661fd8cb38a72c3811cf696
pranays2023 <pranay@wednesday.is>
2023-08-29 18:22:36 +0530

    first commit

---
+++ dbt_gx/aws_resources/main.tf
@@ -0,0 +1,121 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
+}
+
+provider "random" {}
+
+variable "email" {
+  description = "Email address for the SNS subscription"
+  type        = string
+}
+
+##IAM
+
+resource "aws_iam_role" "common_iam_role" {
+  name = "common_iam_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = ["ec2.amazonaws.com", "lambda.amazonaws.com"]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "common_iam_role_policy_attachment" {
+  role       = aws_iam_role.common_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+##S3
+
+resource "random_pet" "bucket_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "${random_pet.bucket_name.id}-test"
+
+  tags = {
+    Name        = "${random_pet.bucket_name.id}-test"
+    Environment = "Dev"
+  }
+}
+
+##SNS
+
+resource "random_pet" "sns_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "aws_sns_topic" "my_topic" {
+  name = "${random_pet.sns_name.id}-sns-topic"
+}
+
+resource "aws_sns_topic_subscription" "my_topic_subscription" {
+  topic_arn = aws_sns_topic.my_topic.arn
+  protocol  = "email"
+  endpoint  = var.email
+}
+
+##Lambda
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "notifysns"
+  role          = aws_iam_role.common_iam_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  filename      = "lambda_function.zip"
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.my_topic.arn
+    }
+  }
+
+  tags = {
+    Name = "notifysns"
+  }
+}
+
+
+resource "aws_lambda_function_event_invoke_config" "destination_config" {
+  function_name = aws_lambda_function.lambda_function.function_name
+  destination_config {
+    on_success {
+      destination = aws_sns_topic.my_topic.arn
+    }
+  }
+}
+
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.my_topic.arn
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.lambda_function.arn
+}
---
+++ dbt_gx/dbt_workspace/main.tf
@@ -0,0 +1,259 @@
+variable "databricks_account_username" {
+    default = "pranay@wednesday.is"
+}
+variable "databricks_account_password" {
+    default = "Sadhguru@2023"
+}
+variable "databricks_account_id" {
+    default = "01557807-7345-4a18-9425-1d31ee2294a5"
+}
+
+variable "tags" {
+  default = {}
+}
+
+variable "cidr_block" {
+  default = "10.4.0.0/16"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+resource "random_string" "naming" {
+  special = false
+  upper   = false
+  length  = 6
+}
+
+locals {
+  prefix = "demo${random_string.naming.result}"
+}
+
+resource "time_sleep" "wait" {
+  depends_on = [
+    aws_iam_role.cross_account_role
+  ]
+  create_duration = "20s"
+}
+
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+// initialize provider in "MWS" mode to provision new workspace
+provider "databricks" {
+  alias    = "mws"
+  host     = "https://accounts.cloud.databricks.com"
+  username = var.databricks_account_username
+  password = var.databricks_account_password
+}
+
+
+data "databricks_aws_assume_role_policy" "this" {
+  external_id = var.databricks_account_id
+}
+
+resource "aws_iam_role" "cross_account_role" {
+  name               = "${local.prefix}-crossaccount"
+  assume_role_policy = data.databricks_aws_assume_role_policy.this.json
+  tags               = var.tags
+}
+
+data "databricks_aws_crossaccount_policy" "this" {
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = "${local.prefix}-policy"
+  role   = aws_iam_role.cross_account_role.id
+  policy = data.databricks_aws_crossaccount_policy.this.json
+}
+
+resource "databricks_mws_credentials" "this" {
+  provider         = databricks.mws
+  account_id       = var.databricks_account_id
+  role_arn         = aws_iam_role.cross_account_role.arn
+  credentials_name = "${local.prefix}-creds"
+  depends_on = [
+    time_sleep.wait
+  ]
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  name = local.prefix
+  cidr = var.cidr_block
+  azs  = data.aws_availability_zones.available.names
+  tags = var.tags
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  create_igw           = true
+
+  public_subnets = [cidrsubnet(var.cidr_block, 3, 0)]
+  private_subnets = [cidrsubnet(var.cidr_block, 3, 1),
+  cidrsubnet(var.cidr_block, 3, 2)]
+
+  manage_default_security_group = true
+  default_security_group_name   = "${local.prefix}-sg"
+
+  default_security_group_egress = [{
+    cidr_blocks = "0.0.0.0/0"
+  }]
+
+  default_security_group_ingress = [{
+    description = "Allow all internal TCP and UDP"
+    self        = true
+  }]
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "3.2.0"
+
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [module.vpc.default_security_group_id]
+
+  endpoints = {
+    s3 = {
+      service      = "s3"
+      service_type = "Gateway"
+      route_table_ids = flatten([
+        module.vpc.private_route_table_ids,
+      module.vpc.public_route_table_ids])
+      tags = {
+        Name = "${local.prefix}-s3-vpc-endpoint"
+      }
+    },
+    sts = {
+      service             = "sts"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags = {
+        Name = "${local.prefix}-sts-vpc-endpoint"
+      }
+    },
+    kinesis-streams = {
+      service             = "kinesis-streams"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags = {
+        Name = "${local.prefix}-kinesis-vpc-endpoint"
+      }
+    },
+  }
+
+  tags = var.tags
+}
+
+resource "databricks_mws_networks" "this" {
+  provider           = databricks.mws
+  account_id         = var.databricks_account_id
+  network_name       = "${local.prefix}-network"
+  security_group_ids = [module.vpc.default_security_group_id]
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+}
+
+
+resource "aws_s3_bucket" "root_storage_bucket" {
+  bucket = "${local.prefix}-rootbucket"
+  acl    = "private"
+  force_destroy = true
+  tags = merge(var.tags, {
+    Name = "${local.prefix}-rootbucket"
+  })
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "root_storage_bucket" {
+  bucket = aws_s3_bucket.root_storage_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "root_storage_bucket" {
+  bucket                  = aws_s3_bucket.root_storage_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on              = [aws_s3_bucket.root_storage_bucket]
+}
+
+data "databricks_aws_bucket_policy" "this" {
+  bucket = aws_s3_bucket.root_storage_bucket.bucket
+}
+
+resource "aws_s3_bucket_policy" "root_bucket_policy" {
+  bucket     = aws_s3_bucket.root_storage_bucket.id
+  policy     = data.databricks_aws_bucket_policy.this.json
+  depends_on = [aws_s3_bucket_public_access_block.root_storage_bucket]
+}
+
+resource "aws_s3_bucket_versioning" "root_bucket_versioning" {
+  bucket = aws_s3_bucket.root_storage_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "databricks_mws_storage_configurations" "this" {
+  provider                   = databricks.mws
+  account_id                 = var.databricks_account_id
+  bucket_name                = aws_s3_bucket.root_storage_bucket.bucket
+  storage_configuration_name = "${local.prefix}-storage"
+}
+
+resource "databricks_mws_workspaces" "this" {
+  provider       = databricks.mws
+  account_id     = var.databricks_account_id
+  aws_region     = var.region
+  workspace_name = local.prefix
+
+  credentials_id           = databricks_mws_credentials.this.credentials_id
+  storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
+  network_id               = databricks_mws_networks.this.network_id
+
+  token {
+    comment = "Terraform"
+  }
+}
+
+output "databricks_host" {
+  value = databricks_mws_workspaces.this.workspace_url
+}
+
+output "databricks_token" {
+  value     = databricks_mws_workspaces.this.token[0].token_value
+  sensitive = true
+}
+
+data "databricks_current_user" "me" {
+  depends_on = [databricks_mws_workspaces.this]
+}
+
+
+###aws_resources###
+
+
